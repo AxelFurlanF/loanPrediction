6b2262baf8682a0506fcc93f9e1cb4644732ee34
Axel <axel.furlan95@gmail.com>
2018-03-14 11:48:36 -0300

    first commit

---
+++ Loan.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 14 11:40:51 2018
+
+@author: axelf
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#datasets
+train = pd.read_csv("train.csv")
+test = pd.read_csv("test.csv")
